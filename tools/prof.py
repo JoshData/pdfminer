9b3f4afaa16c833c6cee49c64de6fad213b7bb30
Matt Swain <m.swain@me.com>
2013-05-13 13:22:08 +0100

    Switch to profile module instead of hotshot

--- tools/prof.py
+++ tools/prof.py
@@ -1,36 +1,22 @@
 #!/usr/bin/env python2
 
-import sys
+try:
+    import cprofile as profile
+except ImportError:
+    import profile
+import pstats
 
 
-def prof_main(argv):
-    import hotshot
-    import hotshot.stats
-
-    def usage():
-        print 'usage: %s module.function [args ...]' % argv[0]
-        return 100
-    args = argv[1:]
-    if len(args) < 1:
-        return usage()
-    name = args.pop(0)
-    prof = name + '.prof'
-    i = name.rindex('.')
-    (modname, funcname) = (name[:i], name[i + 1:])
-    module = __import__(modname, fromlist=1)
-    func = getattr(module, funcname)
-    if args:
-        args.insert(0, argv[0])
-        prof = hotshot.Profile(prof)
-        prof.runcall(lambda: func(args))
-        prof.close()
-    else:
-        stats = hotshot.stats.load(prof)
-        stats.strip_dirs()
-        stats.sort_stats('time', 'calls')
-        stats.print_stats(1000)
-    return
+def profile_function(module, func, args):
+    """Profile a function"""
+    module = __import__(module, fromlist=1)
+    func = getattr(module, func)
+    profile.runctx('func(%s)' % args, globals(), locals(), 'stats')
+    p = pstats.Stats('stats')
+    p.strip_dirs().sort_stats('cumulative').print_stats()
 
 
 if __name__ == '__main__':
-    sys.exit(prof_main(sys.argv))
+    path = '../samples/nonfree/i1040nr.pdf'
+    outpath = '../samples/nonfree/i1040nrall.txt'
+    profile_function('pdfminer.pdf2txt', 'main', "['%s', '-p', '1', '2', '3', '4', '-V', '-o', '%s']" % (path, outpath))
